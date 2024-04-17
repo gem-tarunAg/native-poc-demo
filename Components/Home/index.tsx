6ad99de5731462d64c79678acf94d53043ecb064
Tarun Agrawal <Tarun.Agrawal@Geminisolutions.com>
2024-04-17 18:08:10 +0530

    inital commit

---
+++ Components/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { StylesHomePage as styles } from '../../Styles/Home';
+
+function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello This is a Home Page!</Text>
+    </View>
+  );
+}
+
+export default Home;
