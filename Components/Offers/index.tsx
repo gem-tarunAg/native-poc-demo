ccc56f61c7b3e0a6e43b86c23d09dc0ba1a3d234
gem-swapnil <swapnil.mahendru@geminisolutions.com>
2024-04-20 23:19:10 +0530

    Footer added in offer component

--- Components/Offers/index.tsx
+++ Components/Offers/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { StylesOffersPage as styles } from '../../Styles/Offers';
 import { View, Text } from 'react-native';
+import Footer from '../Footer';
 
 function Offers() {
+  let OffersFooterStyle = {
+    position:'absolute',
+    bottom:0,
+    width:'100%'
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.text}>This is a Offers Page</Text>
+      <Footer style={OffersFooterStyle} />
     </View>
   );
 }
