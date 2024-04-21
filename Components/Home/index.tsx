68c8a51a0b94e84c6d03aa6ca1a7fa5b93368181
Tarun Agrawal <Tarun.Agrawal@Geminisolutions.com>
2024-04-21 15:22:50 +0530

    feat: gradient implemented

--- Components/Home/index.tsx
+++ Components/Home/index.tsx
@@ -2,17 +2,14 @@ import React from 'react';
 import {
   View,
   Text,
+  TouchableOpacity,
   ImageBackground,
   StatusBar,
-  TouchableOpacity,
 } from 'react-native';
 import { StylesHomePage as styles } from '../../Styles/Home';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../helpers/Screens';
-
-const image = {
-  uri: 'https://img.freepik.com/free-photo/yong-readhead-bearded-hipster-with-backpack-holding-retro-camera-listening-music_171337-7893.jpg?t=st=1713423764~exp=1713427364~hmac=6500b47407173a0c5addadfd4c0c358e5dcda3e4864385c5e763a27c52f384c6&w=360',
-};
+import { LinearGradient } from 'expo-linear-gradient';
 
 type HomeProps = {
   navigation: NativeStackNavigationProp<RootStackParamList>;
@@ -31,19 +28,28 @@ const Home: React.FC<HomeProps> = ({ navigation }) => {
         backgroundColor="transparent"
         barStyle="light-content"
       />
-      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-        <View style={styles.container3}>
-          <View style={styles.container2}>
-            <Text style={styles.text2}>Bossini</Text>
-          </View>
-          <Text style={styles.boldText}>20% STUDENT OFFER</Text>
-          <Text style={styles.boldText}>AT BOSSINI</Text>
-          <Text style={styles.text}>06 JAN - 01 JUN</Text>
-          <TouchableOpacity style={styles.button} onPress={onViewOffers}>
-            <Text style={styles.buttonText}>View all offers →</Text>
-          </TouchableOpacity>
+      <ImageBackground
+        source={require('../../assets/bg-image.png')}
+        style={styles.imageBackground}
+      />
+      <View style={styles.blackBackground} />
+      <View style={styles.shadowLayer}>
+        <LinearGradient
+          colors={['transparent', 'rgba(0, 0, 0, 0.9)']} // Gradient for shadow
+          style={styles.shadowGradient}
+        />
+      </View>
+      <View style={styles.container3}>
+        <View style={styles.container2}>
+          <Text style={styles.text2}>Bossini</Text>
         </View>
-      </ImageBackground>
+        <Text style={styles.boldText}>20% STUDENT OFFER</Text>
+        <Text style={styles.boldText}>AT BOSSINI</Text>
+        <Text style={styles.text}>06 JAN - 01 JUN</Text>
+        <TouchableOpacity style={styles.button} onPress={onViewOffers}>
+          <Text style={styles.buttonText}>View all offers →</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
--- Styles/Home.tsx
+++ Styles/Home.tsx
@@ -2,12 +2,14 @@ import { StyleSheet } from 'react-native';
 export const StylesHomePage = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black'
   },
-  image: {
-    flex: 1,
-    justifyContent: 'center',
-    zIndex: -1,
+  blackBackground: {
+    flex: 0.2, // Cover bottom 20%
+    backgroundColor: 'black',
+  },
+  imageBackground: {
+    flex: 0.8,
+    resizeMode:'cover',
   },
   container2: {
     alignItems: 'center',
@@ -23,7 +25,12 @@ export const StylesHomePage = StyleSheet.create({
   },
   boldText: { fontSize: 25, fontWeight: 'bold', marginTop: 10, color: 'white' },
   container3: {
+    position: 'absolute',
+    top: '50%',
+    width:'100%',
     alignItems: 'center',
+    marginBottom: 50,
+    zIndex: 7,
   },
   text: {
     color: 'white',
@@ -39,5 +46,18 @@ export const StylesHomePage = StyleSheet.create({
   },
   buttonText: {
     color: 'white',
+  },
+  shadowLayer: {
+    position: 'absolute',
+    top: '50%',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'transparent',
+    zIndex: 6,
+  },
+  shadowGradient: {
+    flex: 1,
+    justifyContent: 'flex-end', // Align gradient to bottom
   },
 });
